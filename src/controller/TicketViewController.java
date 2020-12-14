fc7bf91db100e25583241dff7cc51282edcf5322
MaxGem <49460072+MaxGem@users.noreply.github.com>
2020-12-13 22:21:15 -0500

    added controller for ticket view

---
+++ src/controller/TicketViewController.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+
+/**
+ *
+ * @author Malcolm Gemmell
+ */
+public class TicketViewController {
+
+    @FXML
+    private TableView<?> ticketTable;
+
+    @FXML
+    private Label ticketLabel;
+
+    @FXML
+    private Button purchaseButton;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    void backButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void nextView(ActionEvent event) {
+
+    }
+}
+    
